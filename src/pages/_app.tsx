eea2290881ca3855f55bc9fed62d331301489abc
Siva Addanki <34810623+avsssai@users.noreply.github.com>
2023-03-05 23:38:06 +0530

    basic landing done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Oranienbaum } from "next/font/google";
 
+const oranienbaum = Oranienbaum({
+	subsets: ["latin"],
+	weight: ["400"],
+	variable: "--font-main",
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<main className={`${oranienbaum.variable} font-serif`}>
+			<Component {...pageProps} />
+		</main>
+	);
 }
